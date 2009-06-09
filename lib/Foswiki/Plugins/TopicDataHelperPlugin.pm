7d1b842f595fb878043a158cd3af16cd4d89147d
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-06-09 00:17:24 +0000

    Item1695: port TopicDataHelperPlugin (remove T(m)Wiki cruft)
    git-svn-id: http://svn.foswiki.org/trunk/TopicDataHelperPlugin@4060 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicDataHelperPlugin.pm
+++ lib/Foswiki/Plugins/TopicDataHelperPlugin.pm
@@ -20,7 +20,7 @@ use Foswiki::Func;
 use vars qw($VERSION $RELEASE $pluginName $debug
 );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
+# This should always be $Rev$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
 $VERSION = '$Rev$';
